a2b75c243cdfe0422aaf254fa05ebd3b631aea22
jeremyBass <jeremybass@cableone.net>
2014-05-02 13:03:28 -0700

    starts with a simple connection test by default

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+	var secret=grunt.file.readJSON('secret.json');
+	var privateKey=grunt.file.read(secret.privateKey);
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		secret: grunt.file.readJSON('secret.json'),	  
+		privateKey:'<%= secret.host %>',
+		sshexec: {
+			test: {
+				command: 'touch testfile.txt',
+				options: {
+					host: '<%= secret.host %>',
+					username: '<%= secret.username %>',
+					privateKey: privateKey
+				}
+			}
+		}
+	});
+	
+	grunt.loadNpmTasks('grunt-ssh');
+	grunt.registerTask('default', ['sshexec:test']);
+};
+
+
